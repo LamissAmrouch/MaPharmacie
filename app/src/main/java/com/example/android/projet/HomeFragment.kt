190ad112948bf46dd98665f0cf2e85a9ae7efcd9
LamissAmrouch <32915792+LamissAmrouch@users.noreply.github.com>
2019-08-07 14:38:23 +0200

    commit2

---
+++ app/src/main/java/com/example/android/projet/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.android.projet
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        commencer.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_homeFragment_to_fragment1)
+        }
+    }
+
+}
--- app/src/main/java/com/example/android/projet/MainActivity.kt
+++ app/src/main/java/com/example/android/projet/MainActivity.kt
@@ -3,10 +3,21 @@ package com.example.android.projet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nav = findNavController(R.id.navHost)
+        NavigationUI.setupActionBarWithNavController(this,nav)
     }
+
+
+
+
+
 }
