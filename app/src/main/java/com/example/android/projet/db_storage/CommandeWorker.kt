2ae417ebc3a4d2838a365af4a68d86d3654c4aad
Wididou <fw_dekkiche@esi.dz>
2019-08-19 16:15:27 +0100

    Add worker for each entitie

---
+++ app/src/main/java/com/example/android/projet/db_storage/CommandeWorker.kt
@@ -0,0 +1,53 @@
+package com.example.android.projet.db_storage
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
+import androidx.work.ListenableWorker
+import androidx.work.WorkerParameters
+import androidx.work.impl.utils.futures.SettableFuture
+import com.example.android.projet.entities.Commande
+import com.example.android.projet.entities.Pharmacie
+import com.example.android.projet.local_storage.RoomService
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.android.synthetic.main.fragment_inscrire.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.sql.Timestamp
+
+class CommandeWorker(val ctx: Context, val workParamters: WorkerParameters) : ListenableWorker(ctx, workParamters) {
+
+    lateinit var  future:SettableFuture<Result>
+
+    @SuppressLint("RestrictedApi")
+    override fun startWork(): ListenableFuture<Result> {
+        val future = SettableFuture.create<Result>()
+        val commande = RoomService.appDatabase.getCommandeDAO().getCommandeToSynchronize()
+        addCommande(commande)
+        return future
+    }
+
+    private fun addCommande(commande: Commande) {
+        val call = RetrofitService.endpoint.addCommande(commande)
+        call.enqueue(object : Callback<String> {
+            @SuppressLint("RestrictedApi")
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                future.set(Result.retry())
+            }
+            @SuppressLint("RestrictedApi")
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    commande.isSynchronized=1
+                    RoomService.appDatabase.getCommandeDAO().updateCommande(commande)
+                    future.set(Result.success())
+                    Toast.makeText(ctx,"works!",Toast.LENGTH_SHORT).show()
+
+                } else {
+                    future.set(Result.retry())
+
+                }
+            }
+        })
+    }
+}
